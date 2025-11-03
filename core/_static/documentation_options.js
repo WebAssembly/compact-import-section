713293383dd6f8784e067c32b4dd0e3a2ef6f1a2
bvisness <bvisness@users.noreply.github.com>
2025-11-03 21:48:02 +0000

    deploy: 88beb71ec25709d0973e7ae174e1485abeb87dd3

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 + compact-import-section (Draft 2024-07-18)',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.0 + compact-import-section (2025-11-03)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
