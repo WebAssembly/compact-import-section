d70f2ef02956dca0cfb2b37913dbc36a02a8b5f3
bvisness <bvisness@users.noreply.github.com>
2025-11-06 18:07:08 +0000

    deploy: 1f1525ee7ce7e3897aace09bfb1061cf585f2fd8

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 + compact-import-section (2025-11-03)',
+    VERSION: '3.0 (2025-11-06)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
